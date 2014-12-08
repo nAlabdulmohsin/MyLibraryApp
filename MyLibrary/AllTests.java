73e848f6be280488523379b3318e39f6b8562f49
aktunc-STMU <aktunc@gmail.com>
2014-12-07 20:17:57 -0600

    Update AllTests.java

--- MyLibrary/AllTests.java
+++ MyLibrary/AllTests.java
@@ -5,7 +5,7 @@ import junit.framework.TestSuite;
 
 public class AllTests {
 
-	public static Test suite() {
+	public static Test suite() { //add MyLibraryTest to the test suite
 		TestSuite suite = new TestSuite(AllTests.class.getName());
 		//$JUnit-BEGIN$
 		suite.addTestSuite(BookTest.class);
